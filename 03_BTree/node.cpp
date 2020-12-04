8d37a2aebc5b34d3073a5dc30104f203437b97eb
Vadim <varem99@mail.ru>
2020-12-04 23:21:43 +0300

    insert, simple search

--- 03_BTree/node.cpp
+++ 03_BTree/node.cpp
@@ -16,6 +16,15 @@ Node::Node(int degree):
 	std::cout << "Node constructed!\ndegree = " << degree_ << std::endl;
 }
 
+void Node::shift(int64_t key, int64_t idx)
+{
+	for(int i(idx + 2); i < int64_t(keyNum_ + 1); ++i)
+	{
+		data_[i] = data_[i - 1];
+	}
+	data_[idx + 1].key_ = key;
+}
+
 void Node::insert(int64_t key)
 {
 	if(keyNum_ == 0)
@@ -24,16 +33,37 @@ void Node::insert(int64_t key)
 		keyNum_++;
 		std::cout << "Key " << key << " inserted!\n";
 	}
-	if((keyNum_ > 0) && (keyNum_ < degree_ * 2)){
-		if(key > data_[keyNum_ - 1].key_)
+	if((keyNum_ > 0) && (keyNum_ < degree_ * 2 - 1)){
+		for(int64_t i(keyNum_ - 1); i >= 0; --i)
 		{
-			data_[keyNum_].key_ = key;
-			keyNum_++;
-			std::cout << "Key " << key << " inserted!\n";
+			if(key > data_[i].key_)
+			{
+				shift(key, i);
+				std::cout << "Shift case! Key " << key << " inserted!\n";
+			}
 		}
-		else
+		
+	}
+}
+
+void Node::split(Node* child, int idx)
+{
+
+}
+
+
+Node* Node::search(int64_t key)
+{
+	for(int64_t i(0); i < keyNum_; ++i)
+	{
+		if(key == data_[i].key_)
 		{
-			// split
+			std::cout << "key " << key << " found!\n";
+			return this;
 		}
 	}
+
+	// if leaf -> return nullptr;
+	// return data_[keyNum_ - 1].child_->search(key);
+	return nullptr;
 }
--- 03_BTree/node.hpp
+++ 03_BTree/node.hpp
@@ -1,3 +1,5 @@
+#ifndef NODE_HPP
+#define NODE_HPP
 #include <vector>
 #include <string>
 
@@ -17,6 +19,9 @@ public:
 	~Node() = default;
 	Node(int degree);
 	void insert(int64_t key);
+	void split(Node* child, int idx);
+	Node* search(int64_t key);
+	void shift(int64_t key, int64_t idx);
 private:
 	uint32_t degree_;
 	uint32_t keyNum_;
@@ -24,3 +29,5 @@ private:
 	bool isLeaf_;
 	std::vector<Pair> data_;
 };
+
+#endif // NODE_HPP
